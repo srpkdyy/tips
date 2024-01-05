e3b75217846c722a3d7d9cd24a981ed14ad5a2d7
srpkdyy <srpkdyy@gmail.com>
2024-01-05 20:26:32 +0900

    add functions

---
+++ algorithm/functions.cpp
@@ -0,0 +1,15 @@
+long long floor(long long x, long long m) {
+   return x - ((x % m) + m) % m;
+}
+
+
+long long pow(long long x, long long n, long long m) {
+   long long ret = 1;
+   while (n) {
+      if (n & 1) ret = (ret * x) % m;
+      x = (x * x) % m;
+      n >>= 1;
+   }
+   return ret;
+}
+
